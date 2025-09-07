5a701eb2be68bee8d8014403a9d9ea1b98bdb0f4
Anmol Sinha <anmolsinha@Anmols-MacBook-Air.local>
2025-09-07 15:26:24 +0530

    add hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,21 @@
 class Hello{
     public static void main(String a[]){
-    System.out
-    .println("Hello, World!");
+        int num1=9;
+        byte by=127;
+        short sh=32767;
+        long lo=123456789L;
+        float fl=3.14f;
+        double d=3.14;
+        char ch='A';
+        boolean b=true;
+        System.out.println(num1);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(lo);
+        System.out.println(fl);
+        System.out.println(d);
+        System.out.println(ch);
+        System.out.println(b);
     }
 }
 
